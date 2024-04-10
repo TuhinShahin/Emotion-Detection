b7fbd0e1c0dcfab074db53b3dd6b343fd12ff40f
Md Shahin Mia <tuhin.shahin@yahoo.com>
2024-04-10 21:20:39 +0100

    Update Emotion_detection_test.py

--- Emotion_detection_test.py
+++ Emotion_detection_test.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import streamlit as st
 from keras.models import model_from_json
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 emotion_dict = {0: "angry", 1: "disgusted", 2: "fearful", 3: "happy", 4: "neutral", 5: "sad", 6: "surprised"}
 
@@ -52,6 +54,60 @@ while True:
 
         # Display the predicted emotion text above the bounding box
         cv2.putText(frame, emotion_dict[maxindex], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+def main():
+    # Face Analysis Application #
+    st.title("Real Time Face Emotion Detection Application")
+    activiteis = ["Home", "Webcam Face Detection", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activiteis)
+    st.sidebar.markdown(
+        """ Developed by Mohammad Juned Khan    
+            Email : Mohammad.juned.z.khan@gmail.com  
+            [LinkedIn] (https://www.linkedin.com/in/md-juned-khan)""")
+    if choice == "Home":
+        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                            <h4 style="color:white;text-align:center;">
+                                            Face Emotion detection application using OpenCV, Custom CNN model and Streamlit.</h4>
+                                            </div>
+                                            </br>"""
+        st.markdown(html_temp_home1, unsafe_allow_html=True)
+        st.write("""
+                 The application has two functionalities.
+
+                 1. Realtime Emotion Detection
+
+                 
+
+                 """)
+    elif choice == "Realtime Emotion Detection":
+        st.header("Webcam Live Feed")
+        st.write("Click on start to use webcam and detect your face emotion")
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    elif choice == "About":
+        st.subheader("About this app")
+        html_temp_about1= """<div style="background-color:#6D7B8D;padding:10px">
+                                    <h4 style="color:white;text-align:center;">
+                                    Real time face emotion detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
+                                    </div>
+                                    </br>"""
+        st.markdown(html_temp_about1, unsafe_allow_html=True)
+
+        html_temp4 = """
+                             		<div style="background-color:#98AFC7;padding:10px">
+                             		<h4 style="color:white;text-align:center;">TThis application have developed by Md Shahin Mia, Check in the link to connect with me linkedin (https://www.linkedin.com/in/tuhin-shahin-linkedin/). </h4>
+                             		<h4 style="color:white;text-align:center;">Thanks for getting in touch</h4>
+                             		</div>
+                             		<br></br>
+                             		<br></br>"""
+
+        st.markdown(html_temp4, unsafe_allow_html=True)
+
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
 
     # Display the frame
     cv2.imshow('Emotion Detection', frame)
